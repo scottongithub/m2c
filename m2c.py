6b5f9c9073dcd481fcac4c22b5f00a1213df5165
scottongithub <wherami@gmail.com>
2020-05-21 08:06:22 -0400

    initial commit

---
+++ m2c.py
@@ -0,0 +1,51 @@
+import sys
+from music21 import *
+#from pprint import pprint
+
+infile = str(sys.argv[1])
+bpm = int(input("BPM?: "))
+
+#open MIDI file and map object 'mt' to track 0 of the MIDI file
+mf = midi.MidiFile()
+mf.open(infile)
+mf.read()
+mt = mf.tracks[0].events
+tpq = mf.ticksPerQuarterNote
+ms_per_tick = (60000) / (bpm * tpq)
+
+#MIDI 'pitch' to Hz
+def note_to_freq( _note ):
+    _freq = (2 ** ((_note - 69) / 12)) * 440
+    return(_freq)
+
+#initialize counters
+curr_state = "off"
+curr_pitch = 0
+note_overlap = 0
+note_buffer = 0
+
+#business starts here. the overlap stuff is to accommodate more than one note
+#at a time - it stops the older note and starts the new one
+for event in mt:
+    if isinstance(event.time, int) and (event.time > 0):
+        #all time entries (DeltaTime) get thrown into the buffer
+        note_buffer = note_buffer + event.time
+    elif event.isNoteOn() and curr_state == "off":
+        #must've been whitespace - print it and set info of the new note
+        print(":delay %ims;" % (ms_per_tick * note_buffer))
+        note_buffer = 0
+        curr_state = "on"
+        curr_pitch = event.pitch
+    elif event.isNoteOn() and curr_state == "on":
+        #a new note overlaps - spit out the previous note and start the new one
+        print(":beep frequency=%i" % note_to_freq(curr_pitch), "length=%ims;" % (ms_per_tick * note_buffer))
+        note_buffer = 0
+        note_overlap += 1
+        curr_pitch = event.pitch
+    elif event.isNoteOff() and note_overlap >=1:
+        note_overlap -=1
+    elif event.isNoteOff() and (curr_state) == "on":
+        #must be no overlap so print the previous note and start whitespace
+        print(":beep frequency=%i" % note_to_freq(curr_pitch), "length=%ims;" % (ms_per_tick * note_buffer))
+        note_buffer = 0
+        curr_state = "off"
---
+++ readme.md
@@ -0,0 +1,50 @@
+# Overview
+Takes a MIDI file as input and outputs the corrosponding config statements to get it to play on Omnitik routers
+
+# Usage
+Uses the music21 library from MIT
+```sh
+$ pip3 install music21
+```
+Then run it:
+```sh
+$ python3 m2c.py <filename>
+```
+
+It will ask for BPM - this will be what BPM was set to on whatever you've auditioned the MIDI file on but can be changed (Ardour's default setting is 120). And then it spits out the the config commands for copy-paste into a config file/template.
+
+# Notes
+If multiple notes are sounding, the most recently played note takes over
+It reads 'track 0' of the MIDI file which seems to be what anything I've tested is using
+Suggestions are always welcome and appreciated
+
+# Example
+```sh
+$ python3 m2c.py ~/m2c/take_8.mid
+music21: Certain music21 functions might need these optional packages: matplotlib, numpy, scipy;
+                   if you run into errors, install them by following the instructions at
+                   http://mit.edu/music21/doc/installing/installAdditional.html
+BPM?: 120
+:delay 508ms;
+:beep frequency=130 length=565ms;
+:delay 468ms;
+:beep frequency=146 length=540ms;
+:delay 425ms;
+:beep frequency=164 length=569ms;
+:delay 370ms;
+:beep frequency=174 length=537ms;
+:delay 400ms;
+:beep frequency=195 length=586ms;
+:delay 339ms;
+:beep frequency=130 length=453ms;
+:beep frequency=146 length=433ms;
+:beep frequency=164 length=459ms;
+:beep frequency=174 length=426ms;
+:beep frequency=195 length=441ms;
+:delay 11ms;
+:beep frequency=174 length=398ms;
+:beep frequency=164 length=464ms;
+:beep frequency=146 length=456ms;
+:delay 5ms;
+:beep frequency=130 length=647ms;
+```
